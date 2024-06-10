5385c404bc82f5c65e14a7ddc80a7adc64268153
Mjunajo <crypto0346@gmail.com>
2024-06-10 15:33:34 +0500

    Delete startup.sh

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pip install torch --extra-index-url https://download.pytorch.org/whl/cu113
-pip install diffusers
-pip install streamlit
-pip install safetensors
-pip install transformers
