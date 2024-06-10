274e3fb9a4a4383c24b72b45ca48c105bc1802b1
Mjunajo <crypto0346@gmail.com>
2024-06-10 15:17:59 +0500

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip install torch --extra-index-url https://download.pytorch.org/whl/cu113
+pip install diffusers
+pip install streamlit
+pip install safetensors
+pip install transformers
